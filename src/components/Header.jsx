eef2d76d6fe56e817dba12ffd43bbbbaf5bcae41
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-06-30 23:59:21 +0530

    Update the Header file in the component Folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {FiShoppingBag} from 'react-icons/fi'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <h2>Logo Here...</h2>
+
+      <div>
+        <Link to={'/'}>Home</Link>
+         <Link to={'/cart'}>
+            <FiShoppingBag/>
+            <p>{0}</p>
+         </Link>
+      </div>
+    </nav>
   )
 }
 
