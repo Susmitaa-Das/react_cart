9d9b2636eab5adbf765d8db9ec96693b8731fbbb
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-06-30 21:35:12 +0530

    Added Header and Home files in Components folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './styles/app.scss';
+import './styles/app.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Header from './components/Header'
 function App() {
   return (
-    <div className="App"></div>
-  );
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
