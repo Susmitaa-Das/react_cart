a73577589f00928009b279cd5f23c0c791c13508
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-07-04 23:37:24 +0530

    Added Home scss file in styles folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className="home">
+      <ProductCard/>
+    </div>
   )
+
+  // const ProductCard = ({ name, id, price, handler, imgSrc }) => (
+  //   <div className="productCard">
+  //     <img src={imgSrc} alt={name} />
+  //     <p>{name}</p>
+  //     <h4>${price}</h4>
+  //     <button onClick={() => handler()}>
+  //       Add to Cart
+  //     </button>
+  //   </div>
+  // );
 }
 
 export default Home
