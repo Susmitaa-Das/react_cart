befbd053f1551d08f1b1ba8b2d4ee29f4bf3391b
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-07-08 23:26:19 +0530

    Update header jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {FiShoppingBag} from 'react-icons/fi'
 const Header = () => {
   return (
     <nav>
-      <h2>Logo is here</h2>
+      <h2>Logo here...</h2>
       <h1>The cart logo is here</h1>
 
       <div>
