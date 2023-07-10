783a25d76035a5b7afd36f6574360fce3a023cef
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-07-10 23:11:54 +0530

    Updated Header jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {FiShoppingBag} from 'react-icons/fi'
+import { FiShoppingBag } from 'react-icons/fi'
 
 const Header = () => {
   return (
     <nav>
       <h2>Logo here...</h2>
-      <h1>The cart logo is here</h1>
+      <h1>The logo is here</h1>
 
       <div>
         <Link to={'/'}>Home</Link>
-         <Link to={'/cart'}>
-            <FiShoppingBag/>
-            <p>{0}</p>
-         </Link>
+        <Link to={'/cart'}>
+          <FiShoppingBag />
+          <p>{0}</p>
+        </Link>
       </div>
     </nav>
   )
